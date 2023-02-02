e27ad0566bdeeb473d16b2a8f275beb8fe904b6e
Munezero Christian <chamsf5@aol.com>
2023-02-03 00:35:43 +0200

    Add pagination

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -2,7 +2,11 @@ const Product = require('../models/product');
 
 const getAllProducts = async (req, res) => {
   let queryObject = { ...req.query };
-  const products = await Product.find(queryObject);
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit > 100 ? 100 : req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const products = await Product.find(queryObject).skip(skip).limit(limit);
   res.json({ products, nbHits: products.length });
 };
 
