6520fe0c2a7472055fdb490f7931d3c26b77b579
Munezero Christian <chamsf5@aol.com>
2023-02-02 13:05:49 +0200

    Add a basic controller function to get all products

---
+++ src/controllers/products.js
@@ -0,0 +1,5 @@
+const getAllProducts = async (req, res) => {
+  res.send('Hello from products');
+};
+
+module.exports = { getAllProducts };
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
+const { getAllProducts } = require('../controllers/products');
 
-router.route('/').get((req, res) => {
-  res.send('Hello from products');
-});
+router.route('/').get(getAllProducts);
 
 module.exports = router;
