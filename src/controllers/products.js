4eb0a97babf941dbe6767871b414c650ffcbc667
Munezero Christian <chamsf5@aol.com>
2023-02-03 01:33:47 +0200

    Filtering based on schema fields

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -2,11 +2,21 @@ const Product = require('../models/product');
 
 const getAllProducts = async (req, res) => {
   let queryObject = { ...req.query };
+  const excludedFields = ['page', 'sort', 'limit', 'fields', 'numericFilters'];
+  excludedFields.forEach((el) => delete queryObject[el]);
+
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit > 100 ? 100 : req.query.limit) || 10;
   const skip = (page - 1) * limit;
 
-  const products = await Product.find(queryObject).skip(skip).limit(limit);
+  let result = Product.find(queryObject).skip(skip).limit(limit);
+
+  if (req.query.fields) {
+    const fields = req.query.fields.split(',').join(' ');
+    result = result.select(fields);
+  }
+
+  const products = await result;
   res.json({ products, nbHits: products.length });
 };
 
