14e0b0a7e5cf510877059f0ccbdb04d9502e76d6
Munezero Christian <chamsf5@aol.com>
2023-02-02 12:49:16 +0200

    Add all products route functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/api/v1/products', productsRouter);
 
 const checkConnectionAndStartServer = async () => {
   try {
