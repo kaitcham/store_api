1c169d2beccfc4e360d863aebf02d8b8beeae227
Munezero Christian <chamsf5@aol.com>
2023-02-03 00:32:41 +0200

    Add functionality to query all products

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,5 +1,9 @@
+const Product = require('../models/product');
+
 const getAllProducts = async (req, res) => {
-  res.send('Hello from products');
+  let queryObject = { ...req.query };
+  const products = await Product.find(queryObject);
+  res.json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProducts };
